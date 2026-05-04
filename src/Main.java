f718762c504cd378e7afe851b58b4b43857585b1
Areg Muradyan <aregmuradyan.dev@gmail.com>
2026-05-04 22:47:05 +0400

    auto-save

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
+import cli.CliRenderer;
+import core.GameMap;
+import core.Player;
+import core.Game;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        String mapText = """
+        ############################################################
+        #P.........................................................#
+        #..........................................................#
+        #..........########........................................#
+        #..........................................................#
+        #.....................######...............................#
+        #..........................................................#
+        #......####................................................#
+        #..........................................................#
+        #.........................####.............................#
+        #..........................................................#
+        #..............1...........................................#
+        #.....2.............................................3......#
+        #.......................................................D..#
+        ############################################################
+        """;
+
+        GameMap map = new GameMap(mapText);
+        Player player = new Player(map.getPlayerStart());
+        Game game = new Game(map, player);
+        CliRenderer renderer = new CliRenderer();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            renderer.render(map, player);
+
+            System.out.print("Move with WASD, q to quit: ");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            }
+
+            switch (input.toLowerCase()) {
+                case "w" -> game.movePlayer(-1, 0);
+                case "s" -> game.movePlayer(1, 0);
+                case "a" -> game.movePlayer(0, -1);
+                case "d" -> game.movePlayer(0, 1);
+                default -> System.out.println("Invalid input.");
+            }
+
+            System.out.println();
+        }
     }
 }
--- src/core/Game.java
+++ src/core/Game.java
@@ -1,5 +1,25 @@
 package core;
 
 public class Game {
+    private final GameMap map;
+    private final Player player;
 
+    public Game(GameMap map, Player player) {
+        this.map = map;
+        this.player = player;
+    }
+
+    public void movePlayer(int dRow, int dCol) {
+        Position nextPosition = player.getPosition().move(dRow, dCol);
+
+        if (!map.isInside(nextPosition)) {
+            return;
+        }
+
+        if (map.isWall(nextPosition)) {
+            return;
+        }
+
+        player.setPosition(nextPosition);
+    }
 }
