faac81a520e5da6b07fbb035e40e335485b4e8a6
Areg Muradyan <aregmuradyan.dev@gmail.com>
2026-05-04 21:44:34 +0400

    Position class updated

--- src/core/Position.java
+++ src/core/Position.java
@@ -1,4 +1,8 @@
 package core;
 
-public class Position {
+public record Position(int row, int col) {
+
+    public Position move(int dRow, int dCol) {
+        return new Position(row + dRow, col + dCol);
+    }
 }
