777066fa4ee3da0501c49b30ef9ead5c27b7f328
Areg Muradyan <aregmuradyan.dev@gmail.com>
2026-05-04 22:30:38 +0400

    auto-save

--- src/core/Player.java
+++ src/core/Player.java
@@ -1,4 +1,17 @@
 package core;
 
 public class Player {
+    private Position position;
+
+    public Player(Position startPosition) {
+        this.position = startPosition;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
 }
